8e5d3829c78a9cd3abb206a5ab46c27f0647edaf
Jordan <jlack@jlackPC.(none)>
2014-01-07 15:37:58 -0600

    added in plot of robots foot.

--- inverted_pendulum.m
+++ inverted_pendulum.m
@@ -8,7 +8,7 @@ if nDof == 3
 else
    addpath("build_2dof");
    
-   ic = [pi/2;pi/2;0.3;0.3];
+   ic = [0.1;-0.1;0;0];
 endif
 
 options =odeset("RelTol",1e-3, "AbsTol", 1e-3,"InitialStep",1e-4,"MaxStep",1e-2);
@@ -22,14 +22,22 @@ figure(1);
 leg = plot3(NaN,NaN,NaN,"r","linewidth",5);
 hold on
 comBall = plot3(NaN,NaN,NaN,"o","markersize",20,"linewidth",20);
+hold on
+footRect = plot3(NaN,NaN,NaN,"linewidth",5);
 axis([-1.1 1.1 -1.1 1.1 -1.1 1.1]);
 
 pFoot = [0;0;0];
+
+ankleToeLen = 0.1;
+ankleHeelLen = 0.05;
+footWidth = 0.05;
 
 for i = 1:length(t)
    
     CoM= pCoM(sol(i,1:3));
     legLine = [pFoot,CoM'];
+    foot = [pFoot + [ankleToeLen; footWidth/2; 0],pFoot + [ankleToeLen; -footWidth/2; 0],...
+                  pFoot + [-ankleHeelLen; -footWidth/2; 0], pFoot + [-ankleHeelLen; footWidth/2; 0],pFoot + [ankleToeLen; footWidth/2; 0]];
 
     set(comBall,'XData',CoM(1),'erasemode','normal');    	
     set(comBall,'YData',CoM(2),'erasemode','normal');
@@ -38,6 +46,10 @@ for i = 1:length(t)
     set(leg,'XData',legLine(1,:),'erasemode','normal');
     set(leg,'YData',legLine(2,:),'erasemode','normal');
     set(leg,'ZData',legLine(3,:),'erasemode','normal');    	
+    
+    set(footRect,'XData',foot(1,:),'erasemode','normal');
+    set(footRect,'YData',foot(2,:),'erasemode','normal');
+    set(footRect,'ZData',foot(3,:),'erasemode','normal');
 
     drawnow;
 	
